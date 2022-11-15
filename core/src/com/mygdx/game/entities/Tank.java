3ac941719f3ce2888651a3d46882fe9349780b19
Abhay Chowdhry <abhay21508@iiitd.ac.in>
2022-11-15 16:06:27 +0530

    Starting Toxic Tank

--- core/src/com/mygdx/game/entities/Tank.java
+++ core/src/com/mygdx/game/entities/Tank.java
@@ -1,10 +1,37 @@
 package com.mygdx.game.entities;
 
 abstract class Tank {
+    private static final float SPEED = 50;
     abstract void fire();
     abstract void selectWeapon();
+    abstract void drawTank();
+    private float x, y;
 
-    void move() {
+    public float getX() {
+        return x;
+    }
 
+    public float getY() {
+        return y;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public static float getSPEED() {
+        return SPEED;
+    }
+
+    void move_forward() {
+        x += SPEED;
+    }
+
+    void move_backward() {
+        x -= SPEED;
     }
 }
---
+++ core/src/com/mygdx/game/entities/Toxic.java
@@ -0,0 +1,22 @@
+package com.mygdx.game.entities;
+
+public class Toxic extends Tank{
+
+    float x;
+    float y;
+    // x = super.getSPEED();
+    @Override
+    void fire() {
+
+    }
+
+    @Override
+    void selectWeapon() {
+
+    }
+
+    @Override
+    void drawTank() {
+
+    }
+}
