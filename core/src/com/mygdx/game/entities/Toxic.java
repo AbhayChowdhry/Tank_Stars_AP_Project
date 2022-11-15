f21e8e69347bfd29a04f486244c904b0cf66a241
Abhay Chowdhry <abhay21508@iiitd.ac.in>
2022-11-15 16:15:38 +0530

    Movements Issues

--- core/src/com/mygdx/game/entities/Toxic.java
+++ core/src/com/mygdx/game/entities/Toxic.java
@@ -2,9 +2,23 @@ package com.mygdx.game.entities;
 
 public class Toxic extends Tank{
 
-    float x;
-    float y;
-    // x = super.getSPEED();
+//    float x;
+//    float y;
+//    x = Tank.getSPEED();
+//    Tank tank = new Tank();
+
+    @Override
+    public float getX() {
+        return super.getX();
+    }
+
+    @Override
+    public float getY() {
+        return super.getY();
+    }
+
+    float coord = getX();
+
     @Override
     void fire() {
 
