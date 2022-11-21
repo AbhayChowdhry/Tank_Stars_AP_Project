838300c056894f92eaeb72dc1393ccaba1e772bd
ahselhsa <ashlesha21380@iiitd.ac.in>
2022-11-21 19:32:06 +0530

    making terrain

---
+++ core/src/com/mygdx/game/entities/Play.java
@@ -0,0 +1,17 @@
+package com.mygdx.game.entities;
+
+public class Play {
+    private Player player1;
+    private Player player2;
+    private Terrain terrain;
+
+    Play() {
+        Player player1 = new Player();
+        Player player2 = new Player();
+        Terrain terrain = new Terrain();
+    }
+
+    public void drawTerrain(){
+        int vals = terrain.get
+    }
+}
